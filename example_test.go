4cefea113a55cb0b848a590193da5fb99cf79233
Anders Soh <sohjiaiwei@gmail.com>
2022-08-03 23:51:29 +0800

    add tests for global logger

--- example_test.go
+++ example_test.go
@@ -41,3 +41,21 @@ func TestExampleEmergencyInfo(t *testing.T) {
 
 	logger.Emergency("test message")
 }
+
+func TestExampleInitGlobalLogger(t *testing.T) {
+	config := slog.NewConfig(
+		"https://hooks.slack.com/services/webhook", // replace with webhook url
+		t.Name(),
+		"<!here>",
+	)
+
+	logger := slog.NewLogger(config)
+
+	slog.ReplaceGlobalLogger(logger)
+
+	slog.L().Info("test message")
+}
+
+func TestExampleNoopGlobalLogger(t *testing.T) {
+	slog.L().Info("test message")
+}
