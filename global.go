001955b90ca3cbda971b854e979cb3eb57d1c5c1
Anders Soh <sohjiaiwei@gmail.com>
2022-08-03 23:51:19 +0800

    add global logger func

--- global.go
+++ global.go
@@ -1,1 +1,25 @@
 package slog
+
+import (
+	"sync"
+)
+
+var (
+	_globalMu sync.RWMutex
+	_globalL  = NewNoopLogger()
+)
+
+func ReplaceGlobalLogger(logger *Logger) func() {
+	_globalMu.Lock()
+	prevLogger := _globalL
+	_globalL = logger
+	_globalMu.Unlock()
+	return func() { ReplaceGlobalLogger(prevLogger) }
+}
+
+func L() *Logger {
+	_globalMu.RLock()
+	l := _globalL
+	_globalMu.RUnlock()
+	return l
+}
